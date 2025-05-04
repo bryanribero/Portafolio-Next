e2976d27dcdc5ad823e2dedd0f4acabec010d818
Bryan <bryanribero@hotmail.com>
2025-05-04 05:15:01 -0300

    portafolio next

---
+++ portafolio/app/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center bg-slate-900/90">
+      <div className="text-center space-y-5 max-w-2x1 z-10">
+        <p className="py-1 px-2 bg-zinc-900/40 backdrop-blur-sm font-light rounded-full text-white inline-block">Join 7.000+ Users</p>
+      </div>
+    </div>
+  );
+}
