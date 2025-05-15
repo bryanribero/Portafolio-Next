be8e8c88966ee5463778c2bed387db215f8adc3f
Bryan <bryanribero@hotmail.com>
2025-05-15 20:13:53 -0300

    icono download en CV

--- portafolio/components/gen/sectionAbout.tsx
+++ portafolio/components/gen/sectionAbout.tsx
@@ -20,7 +20,7 @@ export default function SectionsAbout() {
     <div className="max-w-150">
       <About onComplete={() => setShowButton(true)} />
 
-      <div className="text-gray-300 flex justify-center mt-30 h-12 "><a href="/assets/Brayan_Ribero_CV_IT_Eng.pdf" download>
+      <div className="text-gray-300 flex justify-center mt-30 h-12  "><a href="/assets/Brayan_Ribero_CV_IT_Eng.pdf" download>
         {showButton && (
           <motion.button
             initial={{ x: 100, opacity: 0 }}
@@ -31,9 +31,12 @@ export default function SectionsAbout() {
               damping: 12,
               duration: 0.3,
             }}
-            className="border-2 border-gray-400 px-8 py-2 rounded-md cursor-pointer transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-blue-500/50 hover:border-transparent tracking-[.15em]"
-          >
-            CV
+            className="border-2 border-gray-400 px-4 py-2 rounded-md cursor-pointer transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-blue-500/50 hover:border-transparent tracking-[.15em]"
+          ><div className="flex">
+            
+            <p className="ml-3">CV</p>
+            <img className="w-5 h-5 ml-1 mr-2 mt-0.5" src="/icons/download.png" alt="Dowload" />
+            </div>
           </motion.button>
         )}
         </a>
