dd87d0835c2d7e0f6ed3784fea83633ebd67b837
Bryan <bryanribero@hotmail.com>
2025-05-16 21:06:27 -0300

    ajustes del responsive

--- portafolio/app/page.tsx
+++ portafolio/app/page.tsx
@@ -16,8 +16,8 @@ export default function Home() {
   const projectsRef = useRef<HTMLDivElement>(null!); 
 
  return (
-  <div className="min-h-screen w-full bg-black grid lg:grid-cols-[minmax(auto,400px)_1fr] xl:grid-cols-[600px_1fr] gap-4">
-    <div className='flex flex-col justify-between md:ml-10 xl:ml-45 mx-auto md:mx-0 px-4 md:px-0'>
+  <div className="min-h-screen w-full bg-black grid lg:grid-cols-[minmax(auto,400px)_1fr] 2xl:grid-cols-[580px_1fr] gap-4">
+    <div className='flex flex-col justify-between 2xl:ml-25 lg:ml-10 mx-auto  px-4 lg:px-0'>
       <div className='relative z-10 mt-10 md:mt-15'>
         <Name />
       </div>
@@ -32,7 +32,7 @@ export default function Home() {
         <Iconos />
       </div>
     </div>
-    <div className='relative col-span-1 z-10 mt-10 md:mt-15 md:col-start-2 md:row-start-1 max-w-screen-md lg:max-w-500 xl:max-w-150 md:ml-10 lg:ml-0 xl:ml-90 mx-auto md:mx-0 overflow-y-auto custom-scrollbar-hidden px-4 md:px-0'>
+    <div className='relative col-span-1 z-10 mt-10 lg:mt-15 lg:col-start-2 lg:row-start-1  max-w-150  mx-auto  overflow-y-auto custom-scrollbar-hidden px-4 md:px-0'>
       <div id='about' ref={aboutRef}>
         <AboutMotion />
       </div>
@@ -41,7 +41,7 @@ export default function Home() {
           <SkillComponent />
         </div>
       </div>
-      <div id='projects' className='mt-50 absolute top-520 w-full' ref={projectsRef}>
+      <div id='projects' className='mt-50 absolute top-480 w-full' ref={projectsRef}>
         <ProjectsMotion />
       </div>
     </div>
